77b392c42445cd814bd59a05563b902e132ea751
Mason Eyre <mason.deyre@gmail.com>
2020-02-10 19:33:55 -0700

    Calculator made
    So far just the add and subtract work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  let [total, setTotal] = useState(0)
+  let [add, setAdd] = useState(0)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input value={total}/>
+        <br/>
+        <input type='button' value='1' onClick={() => setTotal([total + 1])}/>
+        <input type='button' value='2' onClick={() => setTotal([total + 2])}/>
+        <input type='button' value='3' onClick={() => setTotal([total + 3])}/>
+        <input type="button" value='-' onClick={() => {setAdd((add * -2 / 2) - total); setTotal(0);}}/>
+        <br/>
+        <input type='button' value='4' onClick={() => setTotal([total + 4])}/>
+        <input type='button' value='5' onClick={() => setTotal([total + 5])}/>
+        <input type='button' value='6' onClick={() => setTotal([total + 6])}/>
+        <input type="button" value='+' onClick={() => {setAdd(add + (add =+ total)); setTotal(0);}}/>
+        <br/>
+        <input type='button' value='7' onClick={() => setTotal([total + 7])}/>
+        <input type='button' value='8' onClick={() => setTotal([total + 8])}/>
+        <input type='button' value='9' onClick={() => setTotal([total + 9])}/>
+        <input type="button" value='=' onClick={() => {setTotal(total + (total =+ total)); setTotal(0);}}/>
+        <br/>
+        Total: {add}
+      </form>
     </div>
   );
 }
